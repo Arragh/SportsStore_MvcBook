2fe61f7750bbae7935686d5e146041033fda057e
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2020-12-10 18:29:04 +0300

    Добавьте файлы проекта.

---
+++ SportsStore_MvcBook/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore_MvcBook.Models;
+
+namespace SportsStore_MvcBook.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult List() => View(repository.Products);
+    }
+}
---
+++ SportsStore_MvcBook/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore_MvcBook.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Football", Description = "Какой-то футбол", Price = 25 },
+            new Product { Name = "Surf Board", Description = "Какая-то доска", Price = 179 },
+            new Product { Name = "Running Shoes", Description = "Беговая обувь", Price = 95 }
+        }.AsQueryable();
+    }
+}
